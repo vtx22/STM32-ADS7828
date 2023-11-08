380be883eb7a28fade2dba20b16d85861632d4e5
Fynn Raddatz <fynn.raddatz@gmail.com>
2023-11-08 04:23:39 +0100

    added demo CubeIDE project

---
+++ demo-project/Core/Inc/ADS7828.hpp
@@ -0,0 +1,74 @@
+// Library for Interfacing with the ADS7828 Analog to Digital Converter over I2C
+#ifndef ADS7828_HPP
+#define ADS7828_HPP
+
+#include "stm32f1xx_hal.h"
+
+#ifndef HAL_MAX_DELAY
+#define HAL_MAX_DELAY 100
+#endif
+
+#define ADS7828_CHANNELS 16
+// Defines the command bits for every possible channel selection (Datasheet Table 2)
+// Choice between "Differential" for voltage between two channels or "Single Ended" for voltage to COM
+enum ADS7828_CHANNEL
+{
+	CHANNEL_0_COM = 0b1000, // Channel 0 to COM
+	CHANNEL_1_COM = 0b1100, // Channel 1 to COM
+	CHANNEL_2_COM = 0b1001, // Channel 2 to COM
+	CHANNEL_3_COM = 0b1101, // Channel 3 to COM
+	CHANNEL_4_COM = 0b1010, // Channel 4 to COM
+	CHANNEL_5_COM = 0b1110, // Channel 5 to COM
+	CHANNEL_6_COM = 0b1011, // Channel 6 to COM
+	CHANNEL_7_COM = 0b1111, // Channel 7 to COM
+	CHANNEL_0_1 = 0b0000,	// Channel 0 to 1
+	CHANNEL_2_3 = 0b0001,	// Channel 2 to 3
+	CHANNEL_4_5 = 0b0010,	// Channel 4 to 5
+	CHANNEL_6_7 = 0b0011,	// Channel 6 to 7
+	CHANNEL_1_0 = 0b0100,	// Channel 1 to 0
+	CHANNEL_3_2 = 0b0101,	// Channel 3 to 2
+	CHANNEL_5_4 = 0b0110,	// Channel 5 to 4
+	CHANNEL_7_6 = 0b0111,	// Channel 7 to 6
+};
+
+// Sets AD Power Down Mode
+// See Datasheet (Table 1) for PD Mode Selection
+enum ADS7828_PD_MODE
+{
+	POWER_DOWN = 0b00,	 // Power Down Between A/D Converter Conversions
+	REF_OFF = 0b01,		 // Internal Reference Voltage OFF and A/D Converter ON
+	REF_ON_AD_OFF = 0b10, // Internal Reference ON and A/D Converter OFF
+	REF_ON_AD_ON = 0b11	 // Internal Reference ON and A/D Converter ON
+};
+
+class ADS7828
+{
+public:
+	ADS7828(I2C_HandleTypeDef *hi2c, uint8_t address);
+	ADS7828(I2C_HandleTypeDef *hi2c, uint8_t address, float external_ref_voltage);
+	~ADS7828();
+
+	float read_voltage(ADS7828_CHANNEL channel);
+	uint16_t read_digit(ADS7828_CHANNEL channel);
+
+	void set_ref_voltage_external(float ref_voltage);
+	void set_ref_voltage_internal();
+
+	void set_power_mode(ADS7828_PD_MODE mode);
+	void set_power_mode(ADS7828_PD_MODE mode, bool update_now);
+
+	void set_scaling(ADS7828_CHANNEL channel, float scaling);
+	float get_scaling(ADS7828_CHANNEL channel);
+	void reset_scaling(ADS7828_CHANNEL channel);
+	void reset_scaling();
+
+private:
+	float _scaling[ADS7828_CHANNELS] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}; // Channel Voltage Scaling
+	float _ref_voltage = 2.5;																				 // Using the internal 2.5V reference voltage by default
+	ADS7828_PD_MODE _pd_mode = REF_ON_AD_ON;															 // Current Power Down Mode
+
+	uint8_t _address = 0x48;  // I2C Address
+	I2C_HandleTypeDef *_hi2c; // I2C Handle
+};
+
+#endif // ADS7828_HPP
---
+++ demo-project/Core/Src/ADS7828.cpp
@@ -0,0 +1,173 @@
+#include "ADS7828.hpp"
+
+/**
+ * Constructor for ADS7828 object
+ *
+ * @param hi2c Pointer to an initialized I2C_HandleTypeDef for the I2C commands
+ * @param address I2C address of the device, default is 0x48 for AD0 = AD1 = 0
+ */
+ADS7828::ADS7828(I2C_HandleTypeDef *hi2c, uint8_t address) : _hi2c(hi2c), _address(address)
+{
+}
+
+/**
+ * Constructor for ADS7828 object for usage with external reference voltage.
+ * Implicitly changes the power down mode to disable the internal voltage reference!
+ *
+ * @param hi2c Pointer to an initialized I2C_HandleTypeDef for the I2C commands
+ * @param address I2C address of the device, default is 0x48 for AD0 = AD1 = 0
+ * @param external_ref_voltage The external reference voltage (in Volts) connected to the ADC, should be between 0.05V and 5V
+ */
+ADS7828::ADS7828(I2C_HandleTypeDef *hi2c, uint8_t address, float external_ref_voltage) : _hi2c(hi2c), _address(address)
+{
+	set_ref_voltage_external(external_ref_voltage);
+}
+
+ADS7828::~ADS7828()
+{
+}
+
+/**
+ * Reads the voltage of a specified channel configuration.
+ * Conversion from digits to voltage is done with the set reference voltage!
+ *
+ * @param channel The ADS7828_CHANNEL configuration you want the voltage from
+ * @return Measured ADC Voltage [V] of given channel configuration
+ */
+float ADS7828::read_voltage(ADS7828_CHANNEL channel)
+{
+	return read_digit(channel) * _ref_voltage / 4095.0 * _scaling[channel];
+}
+
+/**
+ * Reads the digit of a specified channel configuration.
+ * ADS7828 has 12 Bit resolution, so values from 0 - 4095!
+ *
+ * @param channel The ADS7828_CHANNEL configuration you want the digit from
+ * @return Measured ADC digit (0 - 4095) of given channel configuration
+ */
+uint16_t ADS7828::read_digit(ADS7828_CHANNEL channel)
+{
+	uint8_t command = 0x00;
+
+	command |= (((uint8_t)channel) << 4);
+	command |= (((uint8_t)_pd_mode) << 2);
+
+	uint8_t data[2] = {0x00, 0x00};
+
+	HAL_I2C_Master_Transmit(_hi2c, (_address << 1), &command, 1, HAL_MAX_DELAY);
+	HAL_I2C_Master_Receive(_hi2c, (_address << 1), data, 2, HAL_MAX_DELAY);
+
+	return (uint16_t)((data[0] << 8) + data[1]);
+}
+
+/**
+ * Set your external reference voltage for operation without the internal reference.
+ * Implicitly switches the power down mode to turn the internal reference OFF!
+ *
+ * @param ref_voltage External reference voltage in [V]
+ */
+void ADS7828::set_ref_voltage_external(float ref_voltage)
+{
+	_ref_voltage = ref_voltage;
+
+	if (_pd_mode == REF_OFF)
+	{
+		return;
+	}
+
+	// If you choose an external voltage reference we have to change the mode accordingly
+	set_power_mode(REF_OFF, true);
+}
+
+/**
+ * Set the reference voltage back to internal (2.5V).
+ * Implicitly switches the power down mode to turn the internal reference ON!
+ *
+ * @param ref_voltage External reference voltage in [V]
+ */
+void ADS7828::set_ref_voltage_internal()
+{
+	_ref_voltage = 2.5;
+
+	// If you choose an internal voltage reference we have to change the mode accordingly
+	set_power_mode(REF_ON_AD_ON, true);
+}
+
+/**
+ * Set the ADC power down mode (see datasheet for more info).
+ * Implicitly switches the reference voltage to internal (2.5V) for modes with REF_ON_x
+ *
+ * @param mode The mode you want to switch to
+ * @param update_now If true, the mode is switched instantly by sending a command. Otherwise mode is changed with next read request!
+ */
+void ADS7828::set_power_mode(ADS7828_PD_MODE mode, bool update_now)
+{
+	_pd_mode = mode;
+
+	// If you choose a mode with internal reference we have to set the voltage back
+	if (mode == REF_ON_AD_OFF || mode == REF_ON_AD_ON)
+	{
+		set_ref_voltage_internal();
+	}
+
+	// To update the mode we have to transmit the command, so just do a random request
+	if (update_now)
+	{
+		read_digit(CHANNEL_0_COM);
+	}
+}
+
+/**
+ * Set the ADC power down mode (see datasheet for more info).
+ * Implicitly switches the reference voltage to internal (2.5V) for modes with REF_ON_x
+ *
+ * @param mode The mode you want to switch to
+ */
+void ADS7828::set_power_mode(ADS7828_PD_MODE mode)
+{
+	set_power_mode(mode, true);
+}
+
+/**
+ * Set the scaling for a channel voltage so that read_voltage returns voltage * scaling
+ *
+ * @param channel The Channel to set the scaling for
+ * @param scaling Scaling Factor that will be multiplied with the voltage
+ */
+void ADS7828::set_scaling(ADS7828_CHANNEL channel, float scaling)
+{
+	_scaling[channel] = scaling;
+}
+
+/**
+ * Get the current voltage scaling for a channel
+ *
+ * @param channel The Channel to get the scaling for
+ */
+float ADS7828::get_scaling(ADS7828_CHANNEL channel)
+{
+	return _scaling[channel];
+}
+
+/**
+ * Reset the scaling for a channel voltage back to 1
+ *
+ * @param channel The Channel to reset the scaling for
+ */
+void ADS7828::reset_scaling(ADS7828_CHANNEL channel)
+{
+	set_scaling(channel, 1);
+}
+
+/**
+ * Reset the scaling for all channels back to 1
+ *
+ */
+void ADS7828::reset_scaling()
+{
+	for (uint8_t c = 0; c < ADS7828_CHANNELS; c++)
+	{
+		reset_scaling(static_cast<ADS7828_CHANNEL>(c));
+	}
+}
